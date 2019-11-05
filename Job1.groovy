22e6617aba488dfd5ab368cd42ec5bbc10c0da4b
carmelocal <55434683+carmelocal@users.noreply.github.com>
2019-11-05 15:02:06 +0100

    Update Job1.groovy

--- Job1.groovy
+++ Job1.groovy
@@ -1,17 +1,25 @@
-package hudson.plugins.git;
+// MyProject-Build.groovy
 
-import hudson.plugins.git.*;
-import jenkins.model.*;
+def gitUrl = "https://github.com/example/project.git"
 
-def scm = new GitSCM("https://github.com/carmelocal/test_repo.git")
-scm.branches = [new BranchSpec("*/master")];
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
-def parent = Jenkins.instance
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Job1")
-job.definition = flowDefinition   
-def spec = "*/2 * * * *"
-hudson.triggers.SCMTrigger newCron = new hudson.triggers.SCMTrigger(spec)
-newCron.start(job, true)
-job.addTrigger(newCron)
-job.save()
-                                                                                                                                                                                                                         parent.reload()
+job("MyProject-Build") {
+    description "Builds MyProject from master branch."
+    parameters {
+        stringParam('COMMIT', 'HEAD', 'Commit to build')
+    }
+    scm {
+        git {
+            remote {
+                url gitUrl.
+                branch "origin/master"
+            }
+            extensions {
+                wipeOutWorkspace()
+                localBranch master
+            }
+        }
+    }
+    steps {
+        shell "Look: I'm building master!"
+    }
+}
