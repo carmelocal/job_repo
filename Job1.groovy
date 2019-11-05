502478f6948b88bfe81b6d0e19855c21b20a997f
carmelocal <55434683+carmelocal@users.noreply.github.com>
2019-11-05 09:31:43 +0100

    Create Job1.groovy

---
+++ Job1.groovy
@@ -0,0 +1,16 @@
+import hudson.plugins.git.*;
+
+          def scm = new GitSCM("https://github.com/carmelocal/test_repo.git")
+          scm.branches = [new BranchSpec("*/master")];
+
+          def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
+
+          def parent = Jenkins.instance
+          def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "New Job")
+
+          job.definition = flowDefinition                                                                                                                                                                                def spec = "*/2 * * * *";
+          hudson.triggers.SCMTrigger newCron = new hudson.triggers.SCMTrigger(spec);
+          newCron.start(job, true);
+          job.addTrigger(newCron);
+          job.save();
+                                                                                                                                                                                                                         parent.reload()
