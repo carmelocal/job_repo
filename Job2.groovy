763859f4145bbc2b566863ce733a7ad0cf2b9fcb
carmelocal <55434683+carmelocal@users.noreply.github.com>
2019-11-05 23:22:29 +0100

    Update Job2.groovy

--- Job2.groovy
+++ Job2.groovy
@@ -1,1 +1,13 @@
+def gitUrl = 'https://github.com/carmelocal/test_repo.git'
 
+job('Job1') {
+    scm {
+        git(gitUrl)
+    }
+    triggers {
+        scm('*/2 * * * *')
+    }
+    steps {
+        
+    }
+}
