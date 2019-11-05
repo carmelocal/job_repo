64c0be8239605a2cb60b01c784273e966c97a5d9
carmelocal <55434683+carmelocal@users.noreply.github.com>
2019-11-05 23:19:21 +0100

    Update Job1.groovy

--- Job1.groovy
+++ Job1.groovy
@@ -1,17 +1,13 @@
-package hudson.plugins.git;
+def gitUrl = 'git://github.com/test/test'
 
-import hudson.plugins.git.*;
-import jenkins.model.*;
-
-def scm = new GitSCM("https://github.com/carmelocal/test_repo.git")
-scm.branches = [new BranchSpec("*/master")];
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
-def parent = Jenkins.instance
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Job1")
-job.definition = flowDefinition   
-def spec = "*/2 * * * *"
-hudson.triggers.SCMTrigger newCron = new hudson.triggers.SCMTrigger(spec)
-newCron.start(job, true)
-job.addTrigger(newCron)
-job.save()
-parent.reload()
+job('test-job') {
+    scm {
+        git(gitUrl)
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        
+    }
+}
