e93b29be224c47039b27ec4c1c1c5df157dbbb1b
carmelocal <55434683+carmelocal@users.noreply.github.com>
2019-11-29 12:14:11 +0100

    Update Job1.groovy

--- Job1.groovy
+++ Job1.groovy
@@ -1,18 +1,24 @@
-pipelineJob('Job1') {
+pipelineJob('Test-dmp') {
   definition {
     cpsScm {
       scm {
         git {
-           branch('master')
+           branch('dev')
              remote{
-                url('https://github.com/carmelocal/test_repo.git')
+                url('https://tfs.fcaitem.com/tfs/manufacturing/DMI_PROJECT/_git/DMI_PROJECT')
+            }
+			      def nodeBuilder = NodeBuilder.newInstance()
+            def pathRestrictions = nodeBuilder.createNode('hudson.plugins.git.extensions.impl.PathRestriction')
+            pathRestrictions.appendNode('includedRegions', 'Folder1/.*')
+            extensions {
+                extensions << pathRestrictions
             }
           }
       }
-      scriptPath("Jenkinsfile")
+      scriptPath("Jenkinsfile2")
     }
   }
   triggers{
-    scm('H/2 * * * *')
+    scm('*/2 * * * *')
   }
 }
